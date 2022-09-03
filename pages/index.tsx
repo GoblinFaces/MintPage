98fcd8d07ea22b4bd4348ced143d2749e6c387f1
GoblinFaces <111095619+GoblinFaces@users.noreply.github.com>
2022-09-03 13:17:08 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -92,6 +92,7 @@ const Home: NextPage = () => {
       <div className={styles.mintInfoContainer}>
         <div className={styles.infoSide}>
           {/* Title of your NFT Collection */}
+          <h1>Thank you for minting out! SOLD OUT!</h1>
           <h1>{contractMetadata?.name}</h1>
           {/* Description of your NFT Collection */}
           <p className={styles.description}>{contractMetadata?.description}</p>
